a1890293e16e9cc3fdd3aeeaa50a6f9b006e8392
zulfauzio <64009520+zulfauzio@users.noreply.github.com>
2020-09-24 16:12:26 +0700

    Initial commit

---
+++ app/src/main/java/com/example/todolist3/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.todolist3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
